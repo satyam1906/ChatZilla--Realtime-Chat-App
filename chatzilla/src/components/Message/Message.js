69e51bb30b07d30e89143088e4a0502625bab053
satyam1906 <satyam9322@gmail.com>
2023-09-29 22:42:56 +0530

    Chat App Finalised Almost

--- chatzilla/src/components/Message/Message.js
+++ chatzilla/src/components/Message/Message.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import './Message.css';
+
 const Message = ({ user, message, classs }) => {
   if (user) {
-    return (
-      <div className={`messageBox ${classs}`}>
-        `${user}:${message}`
-      </div>
-    );
-  } 
-  else {
-    return (
-    <div className={`messageBox ${classs}`}>
-      {`You ${message}`}
-    </div>
-    );
+      return (
+          <div className={`messageBox ${classs}`}  >
+              {`${user}: ${message}`}
+          </div>
+      )
   }
-};
+  
+  else {
+      return (
+          <div className={`messageBox ${classs}`}>
+              {`You: ${message}`}
+          </div>
+      )
+  }
+}   
 
 export default Message;
--- server/index.js
+++ server/index.js
@@ -23,15 +23,15 @@ io.on('connection', (socket) => {
   socket.on('joined', ({ user }) => {
     users[socket.id] = user;
     console.log(`${user} has joined`);
-    socket.broadcast.emit('userJoined', { user: 'Admin:', message: `${users[socket.id]} has joined` });
-    socket.emit('welcome', { user: 'Admin:', message: `Welcome to the chat, ${users[socket.id]}` });
+    socket.broadcast.emit('userJoined', { user: 'Admin', message: `${users[socket.id]} has joined` });
+    socket.emit('welcome', { user: 'Admin', message: `Welcome to the chat, ${users[socket.id]}` });
   });
 
   socket.on('message',({message, id})=>{
     io.emit('sendMessage',{user:users[id],message,id});
   })
 
-  socket.on('disconnect',()=>{
+  socket.on('close',()=>{
     socket.broadcast.emit('leave',{user: 'Admin', message:`${users[socket.id]} has left the chat`}) ;
     console.log(`${users[socket.id]} Left`); 
   })
