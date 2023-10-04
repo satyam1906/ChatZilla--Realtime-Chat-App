fec66466bbc81028c4cd442bcea9dbed1773dbd8
satyam1906 <satyam9322@gmail.com>
2023-10-04 20:39:58 +0530

    Saved some changes in backend before deploy

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const socketIO = require('socket.io');
 const dotenv = require('dotenv');
+const port= process.env.PORT
 
 dotenv.config({ path: './config/config.env' });
 
